99fe98cc5f633c818d34fd546e717ffe557eee3a
StriteR <letsbattle@foxmail.com>
2020-06-03 18:57:02 +0800

    Init

---
+++ Assets/Scripts/Core/TCommon.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class TCommon
+{
+    #region Transform
+    public static bool SetActivate(this MonoBehaviour behaviour, bool active)=>SetActivate(behaviour.gameObject, active);
+    public static bool SetActivate(this Transform tra, bool active)=> SetActivate(tra.gameObject, active);
+    public static bool SetActivate(this GameObject go, bool active)
+    {
+        if (go.activeSelf == active)
+            return false;
+
+        go.SetActive(active);
+        return true;
+    }
+    public static void DestroyChildren(this Transform trans)
+    {
+        int count = trans.childCount;
+        if (count <= 0)
+            return;
+        for (int i = 0; i < count; i++)
+            GameObject.Destroy(trans.GetChild(i).gameObject);
+    }
+    public static void SetParentResetTransform(this Transform source,Transform target)
+    {
+        source.SetParent(target);
+        source.transform.localPosition = Vector3.zero;
+        source.transform.localScale = Vector3.one;
+        source.transform.localRotation = Quaternion.identity;
+    }
+    public static void SetChildLayer(this Transform trans, int layer)
+    {
+        foreach (Transform temp in trans.gameObject.GetComponentsInChildren<Transform>(true))
+            temp.gameObject.layer = layer;
+    }
+    public static Transform FindInAllChild(this Transform trans, string name)
+    {
+        foreach (Transform temp in trans.gameObject.GetComponentsInChildren<Transform>(true))
+            if (temp.name == name) return temp;
+        Debug.LogWarning("Null Child Name:" + name + ",Find Of Parent:" + trans.name);
+        return null;
+    }
+
+    public static T Find<T>(this T[,] array, Predicate<T> predicate)
+    {
+        int length0 = array.GetLength(0);
+        int length1 = array.GetLength(1);
+        for (int i = 0; i < length0; i++)
+            for (int j = 0; j < length1; j++)
+                if (predicate(array[i, j])) return array[i, j];
+        return default(T);
+    }
+
+
+    public static void SortChildByNameIndex(Transform transform, bool higherUpper = true)
+    {
+        List<Transform> childList = new List<Transform>();
+        List<int> childIndexList = new List<int>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            childList.Add(transform.GetChild(i));
+            childIndexList.Add(int.Parse(childList[i].gameObject.name));
+        }
+        childIndexList.Sort((a, b) => { return a <= b ? (higherUpper ? 1 : -1) : (higherUpper ? -1 : 1); });
+
+        for (int i = 0; i < childList.Count; i++)
+        {
+            childList[i].SetSiblingIndex(childIndexList.FindIndex(p => p == int.Parse(childList[i].name)));
+        }
+    }
+
+    #endregion
+    #region Collections/Array Traversal
+    public static T GetIndexKey<T, Y>(this Dictionary<T, Y> dictionary, int index) => dictionary.ElementAt(index).Key;
+    public static Y GetIndexValue<T, Y>(this Dictionary<T, Y> dictionary, int index) => dictionary.ElementAt(index).Value;
+
+    public static List<T> DeepCopy<T>(this List<T> list) where T : struct
+    {
+        List<T> copyList = new List<T>();
+        list.Traversal((T value) => { copyList.Add(value); });
+        return copyList;
+    }
+
+    public static Dictionary<T,Y> DeepCopy<T,Y>(this Dictionary<T,Y> dictionary) where T:struct where Y: struct
+    {
+        Dictionary<T, Y> copyDic = new Dictionary<T, Y>();
+        dictionary.Traversal((T key, Y value) => { copyDic.Add(key, value); });
+        return copyDic;
+    }
+
+    public static Dictionary<T,List<Y>> DeepCopy<T,Y>(this Dictionary<T,List<Y>> dictionary) where T:struct where Y:struct
+    {
+        Dictionary<T, List<Y>> copyDic = new Dictionary<T, List<Y>>();
+        dictionary.Traversal((T key, List<Y> value) => { copyDic.Add(key, value.DeepCopy()); });
+        return copyDic;
+    }
+
+    public static void Traversal<T>(this List<T> list, Action<int, T> OnEachItem,bool shallowCopy=false)
+    {
+        List<T> tempList = shallowCopy ? new List<T>(list) : list;
+        TraversalEnumerableIndex(0, list.Count, (int index) => { OnEachItem(index, tempList[index]); return false; });
+    }
+    public static void Traversal<T>(this List<T> list, Action<T> OnEachItem, bool shallowCopy = false)
+    {
+        List<T> tempList = shallowCopy ? new List<T>(list) : list;
+        TraversalEnumerableIndex(0, list.Count, (int index) => { OnEachItem(tempList[index]); return false; });
+    }
+    public static void TraversalBreak<T>(this List<T> list, Func<T,bool> OnEachItem,bool shallowCopy=false)
+    {
+        List<T> tempList = shallowCopy ? new List<T>(list) : list;
+        TraversalEnumerableIndex(0, list.Count, (int index) => {return OnEachItem(tempList[index]);});
+    }
+    public static void Traversal<T, Y>(this Dictionary<T, Y> dic, Action<T> OnEachKey,bool shallowCopy=false)
+    {
+        Dictionary<T, Y> tempDic = shallowCopy ? new Dictionary<T, Y>(dic) : dic;
+        foreach (T temp in tempDic.Keys)
+            OnEachKey(temp);
+    }
+    public static void Traversal<T, Y>(this Dictionary<T, Y> dic, Action<Y> OnEachValue,bool shallowCopy =false)
+    {
+        Dictionary<T, Y> tempDic = shallowCopy  ? new Dictionary<T, Y>(dic) : dic;
+        foreach (T temp in tempDic.Keys)
+            OnEachValue(tempDic[temp]);
+    }
+    public static void Traversal<T, Y>(this Dictionary<T, Y> dic, Action<T, Y> OnEachPair,bool shallowCopy =false)
+    {
+        Dictionary<T, Y> tempDic = shallowCopy  ? new Dictionary<T, Y>(dic) : dic;
+        foreach (T temp in tempDic.Keys)
+            OnEachPair(temp,tempDic[temp]);
+    }
+    public static void TraversalBreak<T, Y>(this Dictionary<T, Y> dic, Func<Y, bool> OnEachItem, bool shallowCopy = false)
+    {
+        Dictionary<T, Y> tempDic = shallowCopy ? new Dictionary<T, Y>(dic) : dic;
+        foreach (T temp in tempDic.Keys)
+            if (OnEachItem(tempDic[temp]))
+                break;
+    }
+    public static void Traversal<T>(this T[] array, Action<T> OnEachItem)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length; i++)
+            OnEachItem(array[i]);
+    }
+    public static void Traversal<T>(this T[] array, Action<int,T> OnEachItem)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length; i++)
+            OnEachItem(i,array[i]);
+    }
+    public static void Traversal<T>(this T[,] array, Action<T> OnEachItem)
+    {
+        int length0 = array.GetLength(0);
+        int length1 = array.GetLength(1);
+        for (int i = 0; i < length0; i++)
+            for (int j = 0; j < length1; j++)
+                OnEachItem(array[i, j]);
+    }
+    public static void Traversal<T>(this T[,] array, Action<int,int, T> OnEachItem)
+    {
+        int length0 = array.GetLength(0);
+        int length1 = array.GetLength(1);
+        for (int i = 0; i < length0; i++)
+            for (int j = 0; j < length1; j++)
+                OnEachItem(i,j,array[i, j]);
+    }
+
+
+    static void TraversalEnumerableIndex(int index, int count,Func<int,bool> OnItemBreak )
+    {
+        if (count == 0)
+            return;
+        for (int i = 0; i < count; i++)
+        {
+            if (OnItemBreak != null && OnItemBreak(index))
+                break;
+
+            index++;
+            if (index == count)
+                index = 0;
+        }
+    }
+
+    #region Enum
+
+    public static void TraversalEnum<T>(Action<T> enumAction)    //Can't Constraint T to System.Enum?
+    {
+        if (!typeof(T).IsSubclassOf(typeof(Enum)))
+        {
+            Debug.LogError("Can't Traversal EnEnum Class!");
+            return;
+        }
+
+        foreach (object temp in Enum.GetValues(typeof(T)))
+        {
+            if (temp.ToString() == "Invalid")
+                continue;
+            enumAction((T)temp);
+        }
+    }
+    public static List<T> GetEnumList<T>()
+    {
+        if (!typeof(T).IsSubclassOf(typeof(Enum)))
+        {
+            Debug.LogError("Can't Traversal EnEnum Class!");
+            return null;
+        }
+
+        List<T> list = new List<T>();
+        Array allEnums = Enum.GetValues(typeof(T));
+        for (int i = 0; i < allEnums.Length; i++)
+        {
+            if (allEnums.GetValue(i).ToString() == "Invalid")
+                continue;
+            list.Add((T)allEnums.GetValue(i));
+        }
+        return list;
+    }
+    #endregion
+    #endregion
+
+}
---
+++ Assets/TouchInputManager.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInputManager : SingletonMono<TouchInputManager>
+{
+    public TouchCheckBase m_Check { get; private set; }
+    public void Init(TouchCheckBase check)
+    {
+        if (m_Check!=null)
+            m_Check.Disable();
+        m_Check = check;
+        m_Check.Enable();
+    }
+
+    void Update()
+    {
+        if (m_Check == null)
+            return;
+
+        m_Check.Tick(Time.unscaledDeltaTime);
+    }
+}
+public enum enum_TouchCheckType
+{
+    Invalid = -1,
+    TouchLR = 1,
+    TouchDrag = 2,
+    TouchDown = 3,
+}
+public abstract class TouchCheckBase
+{
+    public abstract enum_TouchCheckType m_Type { get; }
+    public abstract void Enable();
+    public abstract void Tick(float deltaTime);
+    public abstract void Disable();
+}
+public class TouchCheckLRJoystick : TouchCheckBase
+{
+    public override enum_TouchCheckType m_Type => enum_TouchCheckType.TouchLR;
+    TouchTracker m_TrackLeft, m_TrackRight;
+    Action<Vector2> OnLeftDelta, OnRightDelta;
+    Func<bool> OnCanSendDelta;
+    Vector2 m_leftDelta, m_rightDelta;
+    public TouchCheckLRJoystick(Action<Vector2> _OnLeftDelta, Action<Vector2> _OnRightDelta, Func<bool> _OnCanSendDelta)
+    {
+        m_leftDelta = Vector2.zero;
+        m_rightDelta = Vector2.zero;
+        OnLeftDelta = _OnLeftDelta;
+        OnRightDelta = _OnRightDelta;
+        OnCanSendDelta = _OnCanSendDelta;
+    }
+    public override void Enable()
+    {
+    }
+    public override void Disable()
+    {
+        m_TrackLeft = null;
+        m_TrackRight = null;
+        m_leftDelta = Vector2.zero;
+        m_rightDelta = Vector2.zero;
+        OnLeftDelta?.Invoke(m_leftDelta);
+        OnRightDelta?.Invoke(m_rightDelta);
+        UIT_JoyStick.Instance.OnDeactivate();
+    }
+    public override void Tick(float deltaTime)
+    {
+        if (UIT_JoyStick.Instance == null)
+            return;
+
+        m_rightDelta = Vector2.zero;
+        int touchCount = Input.touchCount;
+        for (int i = 0; i < touchCount; i++)
+        {
+            Touch t = Input.GetTouch(i);
+            if (t.phase == TouchPhase.Began)
+            {
+                TouchTracker track = new TouchTracker(t);
+                if (m_TrackLeft == null && track.isLeft && track.isDown)
+                {
+                    m_TrackLeft = track;
+                    UIT_JoyStick.Instance.OnActivate(m_TrackLeft.v2_startPos);
+                    m_leftDelta = Vector2.zero;
+                }
+                else if (m_TrackRight == null && !track.isLeft)
+                {
+                    m_TrackRight = track;
+                }
+            }
+            else if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
+            {
+                if (m_TrackLeft != null && t.fingerId == m_TrackLeft.m_Touch.fingerId)
+                {
+                    m_TrackLeft = null;
+                    UIT_JoyStick.Instance.OnDeactivate();
+                    m_leftDelta = Vector2.zero;
+                }
+                if (m_TrackRight != null && t.fingerId == m_TrackRight.m_Touch.fingerId)
+                {
+                    m_TrackRight = null;
+                }
+            }
+            else if (t.phase == TouchPhase.Moved)
+            {
+                if (m_TrackRight != null && t.fingerId == m_TrackRight.m_Touch.fingerId)
+                {
+                    m_TrackRight.Record(t);
+                    m_rightDelta = t.deltaPosition;
+                }
+                else if (m_TrackLeft != null && t.fingerId == m_TrackLeft.m_Touch.fingerId)
+                {
+                    m_TrackLeft.Record(t);
+                    m_leftDelta = UIT_JoyStick.Instance.OnMoved(t.position);
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        m_leftDelta = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        m_rightDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); 
+#endif
+
+        if (OnCanSendDelta != null && !OnCanSendDelta())
+        {
+            m_leftDelta = Vector2.zero;
+            m_rightDelta = Vector2.zero;
+        }
+
+        OnLeftDelta?.Invoke(m_leftDelta);
+        OnRightDelta?.Invoke(m_rightDelta);
+    }
+}
+public class TouchCheckScreenDrag : TouchCheckBase
+{
+    public override enum_TouchCheckType m_Type => enum_TouchCheckType.TouchDrag;
+    Action<bool, Vector2> OnDragDown;
+    Action<Vector2> OnDrag;
+    int m_DragTrackID;
+    public TouchCheckScreenDrag(Action<bool, Vector2> _OnDragDown, Action<Vector2> _OnDrag)
+    {
+        OnDragDown = _OnDragDown;
+        OnDrag = _OnDrag;
+    }
+
+    public override void Enable()
+    {
+        m_DragTrackID = -1;
+    }
+    public override void Tick(float deltaTime)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+            OnDragDown(true, Input.mousePosition);
+        else if (Input.GetMouseButtonUp(0))
+            OnDragDown(false, Input.mousePosition);
+        else if (Input.GetMouseButton(0))
+            OnDrag(Input.mousePosition);
+#endif
+        foreach (Touch touch in Input.touches)
+        {
+            if (m_DragTrackID == -1 && touch.phase == TouchPhase.Began)
+            {
+                m_DragTrackID = touch.fingerId;
+                OnDragDown(true, touch.position);
+            }
+
+            if (m_DragTrackID != touch.fingerId)
+                return;
+
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                m_DragTrackID = -1;
+                OnDragDown(false, touch.position);
+                return;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                OnDrag(touch.position);
+            }
+        }
+    }
+    public override void Disable()
+    {
+        m_DragTrackID = -1;
+        OnDragDown(false, Vector2.zero);
+    }
+}
+public class TouchCheckDown : TouchCheckBase
+{
+    public override enum_TouchCheckType m_Type => enum_TouchCheckType.TouchDown;
+    Action<bool, Vector2> OnDown;
+    public TouchCheckDown(Action<bool, Vector2> _OnDown)
+    {
+        OnDown = _OnDown;
+    }
+
+    public override void Tick(float deltaTime)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+            OnDown(true, Input.mousePosition);
+        else if (Input.GetMouseButtonUp(0))
+            OnDown(false, Input.mousePosition);
+#endif
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                OnDown(false, touch.position);
+            }
+            else if (touch.phase == TouchPhase.Began)
+            {
+                OnDown(true, touch.position);
+            }
+        }
+    }
+
+    public override void Disable()
+    {
+    }
+
+    public override void Enable()
+    {
+    }
+}
+
+public class TouchTracker
+{
+    static float f_halfHorizontal = Screen.width / 2;
+    static float f_halfVertical = Screen.height / 2;
+    const float f_minOffset = 50;
+    public Touch m_Touch { get; private set; }
+    public Vector2 v2_startPos { get; private set; }
+    public bool isLeft => v2_startPos.x < f_halfHorizontal;
+    public bool isDown => v2_startPos.y < f_halfVertical;
+    public bool trackSuccessful;
+    public TouchTracker(Touch touchTrack)
+    {
+        m_Touch = touchTrack;
+        v2_startPos = m_Touch.position;
+        trackSuccessful = false;
+    }
+    public void Record(Touch touchTrack)
+    {
+        m_Touch = touchTrack;
+    }
+}
